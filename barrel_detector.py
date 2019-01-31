28c81f43e3651dcd2f35bdbaadbe989418f0744d
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-01-30 21:47:54 -0800

    Update barrel_detector.py

--- barrel_detector.py
+++ barrel_detector.py
@@ -15,7 +15,7 @@ class BarrelDetector():
         s=np.dot(x,self.weights)
         predict=[]
         for i in range(800*1200):
-            if (s[i])>=-4:
+            if (s[i])>=-3.5:
                 predict.append(1)
             else:
                 predict.append(0)
