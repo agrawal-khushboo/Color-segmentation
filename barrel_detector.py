0e63b10cd8cd9c22fb2ba1436e2e5ba7c3d80063
Khushboo Agrawal <44121866+agrawal-khushboo@users.noreply.github.com>
2019-01-30 21:55:37 -0800

    Update barrel_detector.py

--- barrel_detector.py
+++ barrel_detector.py
@@ -31,7 +31,7 @@ class BarrelDetector():
         for p in props:
             minr,minc,maxr,maxc=p.bbox
             r=(maxr-minr)/(maxc-minc)
-            if r>1:   
+            if r>1.5:   
                 b=[minc,minr,maxc,maxr]
                 boxes.append(b)
 #             if r>1:
